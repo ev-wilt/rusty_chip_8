25f0ebb647158770055b141f7519b150c2527861
Evan Wilt <ev.wilt@gmail.com>
2018-05-08 21:06:08 -0400

    Reads ROM into memory, pulls name from command line

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+
 pub struct Cpu {
     program_counter: u16,
     opcode: u16,
@@ -30,8 +33,14 @@ impl Cpu {
     }
 
     // Loads a game given the location of the file
-    pub fn load_game(self, location: &str) {
-        println!("{} Loaded", location);
+    pub fn load_game(&mut self, location: &str) {
+        let mut game = File::open(location).expect("Game was not found.");
+        let mut buffer = [0; 3584];
+        let buffer_size = game.read(&mut buffer[..]).expect("Error when reading file");
+        
+        for i in 0..buffer_size {
+            self.memory[i + 512] = buffer[i];
+        }
     }
 
     // Executes a single CPU cycle
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod core;
 mod cpu;
 use cpu::Cpu;
+use std::env;
 
 fn main() {
     core::initialize_graphics();
     core::initialize_input();
 
-    let cpu = Cpu::initialize();
-    cpu.load_game("Tetris");
+    let mut cpu = Cpu::initialize();
+    let args: Vec<String> = env::args().collect();
+    cpu.load_game(&args[1]);
 }
